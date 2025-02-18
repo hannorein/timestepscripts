398bb3f7f1a47ca80d5192324e508048dee2985c
Hanno Rein <hanno@hanno-rein.de>
2025-02-18 11:49:47 -0500

    check script

--- check_all.py
+++ check_all.py
@@ -1,13 +1,22 @@
 import sys
 import rebound
 import math
+import warnings
+warnings.filterwarnings("ignore")
+
 tmax = 5e9*math.pi*2.0
 N = 80
-for dt in [6, 12, 24, 48]:
+for a in range(1,len(sys.argv)):
+    if "wh" in sys.argv[a]:
+        dt = float(sys.argv[a][2:])
+        wh = "_whfast"
+    else:
+        dt = float(sys.argv[a])
+        wh = ""
     s = 0.0
     for i in range(N):
         try:
-            sa = rebound.Simulationarchive("/scratch/rein/out_%.1e/out_%.1e_%04d.bin"%(dt, dt, i))
+            sa = rebound.Simulationarchive("/scratch/rein/out%s_%.1e/out_%.1e_%04d.bin"%(wh, dt, dt, i))
             prog = sa.tmax/tmax*100
             s += prog
         except:
@@ -17,5 +26,5 @@ for dt in [6, 12, 24, 48]:
         secleft = max(0.0,(1.-s/N/100.0)*sim.walltime/sim.t*tmax)
     except:
         secleft = math.nan
-    print("%.1e avg  %6.2f%%  (%5.2fh left) "%(dt, s/N, secleft/60/60))
+    print("%.1e avg  %6.2f%%  (%5.2fh left) %s"%(dt, s/N, secleft/60/60, wh))
 
